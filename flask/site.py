73ff48c8a077e625844f59a4eec985baafdc20b1
Hannah Smith <hus@uoregon.edu>
2015-10-24 13:20:43 -0700

    Flask skeleton

---
+++ flask/site.py
@@ -0,0 +1,64 @@
+"""
+Very simple Flask web site, with one page
+displaying a course schedule.
+
+"""
+
+import flask
+from flask import render_template
+from flask import request
+from flask import url_for
+
+import json
+import logging
+
+
+###
+# Globals
+###
+app = flask.Flask(__name__)
+import CONFIG
+
+
+import uuid
+app.secret_key = str(uuid.uuid4())
+app.debug=CONFIG.DEBUG
+app.logger.setLevel(logging.DEBUG)
+
+
+###
+# Pages
+###
+
+@app.route("/")
+@app.route("/index")
+def index():
+  app.logger.debug("Main page entry")
+
+  return flask.render_template('site.html')
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    app.logger.debug("Page not found")
+    flask.session['linkback'] =  flask.url_for("index")
+    return flask.render_template('page_not_found.html'), 404
+
+#################
+#
+# Functions used within the templates
+#
+#################
+
+
+
+
+#############
+
+
+if __name__ == "__main__":
+    import uuid
+    app.secret_key = str(uuid.uuid4())
+    app.debug=CONFIG.DEBUG
+    app.logger.setLevel(logging.DEBUG)
+    app.run(port=CONFIG.PORT)
