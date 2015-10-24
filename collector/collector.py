6f80ab88ee6fd6c0c4b8ff7a18f012bc945dac50
Hannah Smith <hus@uoregon.edu>
2015-10-24 16:34:50 -0700

    commit

--- collector/collector.py
+++ collector/collector.py
@@ -1,17 +1,19 @@
 import twitter, json, StringIO
 from textblob import TextBlob
-                      
+
+
 def main():
-    api = twitter.Api(consumer_key='xiRvwM5u5lgLW4GsfMq1BLxmw',
-                  consumer_secret='A7IPWnxsfTfTccnuyF9TXPXEBwpC6WyEVwkNseJXxv4SVqhncd',
-                  access_token_key='1939224967-lZqH7LpKSs775MCNK2ynZlJ0meUIcDt9QiM5KAt',
-                  access_token_secret='bgelSzudLaRrKaQcIHvXOXvNW7QaoNpEiaLmnGAnvYNmt')
+    api = twitter.API(consumer_key='xiRvwM5u5lgLW4GsfMq1BLxmw',
+                      consumer_secret='A7IPWnxsfTfTccnuyF9TXPXEBwpC6WyEVwkNseJXxv4SVqhncd',
+                      access_token_key='1939224967-lZqH7LpKSs775MCNK2ynZlJ0meUIcDt9QiM5KAt',
+                      access_token_secret='bgelSzudLaRrKaQcIHvXOXvNW7QaoNpEiaLmnGAnvYNmt')
     valueswecareabout = []
     getTweets(api, "adele", ("38.5000","98.0000","3000km"))
     for status in query:
-        valueswecareabout.append((status.text, status.location, status.coordinates, status.geo, status.place))
+        valueswecareabout.append(
+            (status.text, status.location, status.coordinates, status.geo, status.place))
     for tweet in valueswecareabout:
-        print getSentiment(tweet[0])
+        print (getSentiment(tweet[0]))
 
 def getTweets(api, searchterm, geo):
     query = api.GetSearch(term = searchterm, count = 100, geocode = geo)
@@ -19,8 +21,8 @@ def getTweets(api, searchterm, geo):
     
         
 def getSentiment(tweet):
-		value = TextBlob(tweet)
-		return value.sentiment.polarity
-        
-if __name__=='__main__':
+    value = TextBlob(tweet)
+    return value.sentiment.polarity
+
+if __name__ == '__main__':
     main()
