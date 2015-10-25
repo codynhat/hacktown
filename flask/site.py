f60d7c90bd628fe91d489642e330e96decedfd1f
Phillip Kriegel <philkrie@gmail.com>
2015-10-25 02:31:56 -0700

    Fixed site.py

--- flask/site.py
+++ flask/site.py
@@ -58,7 +58,7 @@ def calc_tweets():
     result = []
     c = 0
     while c < 10000:
-        res = es.search(index="index", body={"query": {"match_all": {}}}, doc_type=query, size=1000, from=c)["hits"]["hits"]
+        res = es.search(index="index", body={"query": {"match_all": {}}}, doc_type=query, size=1000)["hits"]["hits"]
         for r in res:
             a = {"lat": r["_source"]["lat"], "lng": r["_source"]["lng"], "wgt": r["_source"]["score"], "name": r["_source"]["name"], "text": r["_source"]["text"]}
             result.append(a)
