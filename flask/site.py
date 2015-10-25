e93d8bf3aed79268cd240848ff51e8597af52f93
Cody Hatfield <cody@hatfieldworks.com>
2015-10-24 17:54:15 -0700

    Added API

--- flask/site.py
+++ flask/site.py
@@ -8,10 +8,11 @@ import flask
 from flask import render_template
 from flask import request
 from flask import url_for
+from flask import jsonify
 
 import json
 import logging
-
+from elasticsearch import Elasticsearch
 
 ###
 # Globals
@@ -24,6 +25,7 @@ app.secret_key = str(uuid.uuid4())
 app.debug=CONFIG.DEBUG
 app.logger.setLevel(logging.DEBUG)
 
+es = Elasticsearch()
 
 ###
 # Pages
@@ -51,8 +53,13 @@ def page_not_found(error):
 
 @app.route("/_tweet_calcs")
 def calc_tweets():
-
-    return jsonify(result)
+    query = request.args['query']
+    res = es.search(index="index", body={"query": {"match_all": {}}}, doc_type=query, size=100)["hits"]["hits"]
+    result = []
+    for r in res:
+        a = {"lat": r["_source"]["lat"], "lng": r["_source"]["lng"], "wgt": r["_source"]["score"]}
+        result.append(a)
+    return jsonify({"result": result})
 
 
 #############
