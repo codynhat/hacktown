b2dc6ddfc70b10f4afc5b408cdb33a204f6bfb0c
TJ LaGrow <tjlagrow@128-223-223-103-wireless.uoregon.edu>
2015-10-25 04:25:56 -0700

    Fuck money get bitches

--- flask/munging.py
+++ flask/munging.py
@@ -1,30 +1,21 @@
-from nltk.corpus import stopwords
 from elasticsearch import Elasticsearch
-import frequency
-
-def deleteCommonWords(badLocationData):
-	theStopwordsDic = set(stopwords.words('english'))
-	locations = []
-
-	for line in badLocationData:
-		#print line
-		a = filter(lambda word: not word in theStopwordsDic,line)
-		if (a != ""):
-			locations.append(a)
-	return locations
+from random import randint
 
 
-def main():
+def randomBadLocations():
 	es = Elasticsearch(['http://107.170.211.113:9200'])
-	res = es.search(index="index", body={"query": {"match_all": {}}}, doc_type="bad", size=20)["hits"]["hits"]
+	size = 100
+	res = es.search(index="index", body={"query": {"match_all": {}}}, doc_type="bad", size=size)["hits"]["hits"]
 	result = []
 	for r in res:
 		a = {"loc": r["_source"]["location"]}
 		loc = a["loc"]
-		result.append(loc)
-	s = deleteCommonWords(result)
-	re = frequency.doTheTuples(s)
-	return re
+		if (loc != ''):
+			result.append(loc)
+	index = randint(0, size - 6)
+	print index
+	print {result[index], result[index+1], result[index+2], result[index+3], result[index+4]}
+
 
 if __name__ == "__main__":
-    main()
+    randomBadLocations()
